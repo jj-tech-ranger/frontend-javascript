2aede1148ac65e45afb7474b4a80386de5899697
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-07 12:03:19 +0300

    Fix task_4: Remove Subjects namespace prefix from main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,11 +1,11 @@
 // @ts-ignore
 import { Subjects } from './subjects/Teacher';
 
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
-export const cTeacher: Subjects.Teacher = { firstName: "John", lastName: "Doe", experienceTeachingC: 10 };
+export const cTeacher: Teacher = { firstName: "John", lastName: "Doe", experienceTeachingC: 10 };
 
 console.log("C++");
 cpp.setTeacher(cTeacher);
