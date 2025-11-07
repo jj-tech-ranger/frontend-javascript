f6f7c94279b8cae0d114cb9683d65a6b7b64a282
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-07 11:58:49 +0300

    Fix task_4: Remove space after colon in Subject.ts type annotations

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,7 +1,7 @@
 namespace Subjects {
     export class Subject {
-        teacher: Teacher;
-        setTeacher(teacher: Teacher) {
+        teacher:Teacher;
+        setTeacher(teacher:Teacher) {
             this.teacher = teacher;
         }
     }
